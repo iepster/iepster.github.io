bbe5c57f4ea3a338853ec067f5365b7698de5c75
Mircea Pop <mirceapop89@gmail.com>
2018-02-03 20:35:13 +0200

    Cleanup

--- assets/js/gViz/_init.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  vis: require('./visualizations/_init.js'),
-  ui: require('./ui/_init.js'),
-  initializers: require('./initializers/_init.js'),
-  shared: require('./shared/_init.js')
-};
-
-
-
--- assets/js/gViz/shared/_init.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  helpers: require('./helpers/_init.js'),
-  visualComponents: require('./visual-components/_init.js')
-};
-
